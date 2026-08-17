7795729a0768a96416b19d50f5dcd1808642c171
chandusoni1 <chandreshsoni8055@gmail.com>
2026-08-17 12:11:20 +0530

    swap two numbers

---
+++ ummid.js
@@ -0,0 +1,10 @@
+// swaping of two number using two variables
+
+let a = 120;
+let b=520;
+let c= a; // this vaiable used to store the value of a otherwise both valuee get same   
+a=b;
+b=c;
+console.log("value of a "+ a,"value of b"+ b);
+
+
