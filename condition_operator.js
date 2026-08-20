19c78e9a2e7189c504b04f3c5b93e2fe425dd6e3
chandusoni1 <chandreshsoni8055@gmail.com>
2026-08-20 19:42:09 +0530

    if_else

---
+++ condition_operator.js
@@ -0,0 +1,15 @@
+// if condition 
+
+// if (condition) {
+    
+// }
+
+//checking for even and odd
+let num = prompt('enter any number');
+if (num%2==0) {
+    console.log(num+"is even number");
+    
+} else {
+    console.log(num+'is odd no.');
+    
+}
