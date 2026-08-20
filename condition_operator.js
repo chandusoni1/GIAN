d513445a0796101820fc371957aebac1626303b1
chandusoni1 <chandreshsoni8055@gmail.com>
2026-08-20 19:46:23 +0530

    positive and negetive

--- condition_operator.js
+++ condition_operator.js
@@ -13,3 +13,15 @@ if (num%2==0) {
     console.log(num+'is odd no.');
     
 }
+
+
+
+//checking for positive and negetive 
+let num = prompt('enter any number');
+if (num>=0) {
+    console.log(num+"is positive number");
+    
+} else {
+    console.log(num+'is negetive no.');
+    
+}
