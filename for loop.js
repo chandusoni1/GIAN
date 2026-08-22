a0c57c0caa6d585593887bb7a0d08c62f566789c
chandusoni1 <chandreshsoni8055@gmail.com>
2026-08-22 13:05:55 +0530

    print sum of 10 natural no.

--- for loop.js
+++ for loop.js
@@ -1,5 +1,11 @@
 // print first 10 natural numbers
-for(i=1;i<=10;i++){
-    console.log(i);
-    
+for (i = 1; i <= 10; i++) {
+  console.log(i);
 }
+
+// print sum of 10 natural numbers
+let sum = 0; // sum initialize
+for (i = 0; i <= 10; i++) {
+  sum += i; // (+) is an arithmatic operator used for addition, sum stores the addded value ,eg -  i=1 ,(0+1=1),sum = 1,i=2(1+2=3), sum=3....
+}
+console.log(sum);
