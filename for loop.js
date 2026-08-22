ad2484eadf75b641933f6ce1ecc720b81dcf175f
chandusoni1 <chandreshsoni8055@gmail.com>
2026-08-22 13:37:41 +0530

    Cube Table

--- for loop.js
+++ for loop.js
@@ -14,19 +14,28 @@ console.log(sum);
 
 // print n natural number with its sum valued
 
-let sum = 0; // intialize the sum 
-let n = parseInt(prompt("enter any digit"));// parseInt  is used to covert srtings in to number
+let sum = 0; // intialize the sum
+let n = parseInt(prompt("enter any digit")); // parseInt  is used to covert srtings in to number
 for (i = 1; i <= n; i++) {
   console.log(i); // this will print the counting of n natural number
-  sum += i; // this will sum the natural numbers and stored under variable name sum 
+  sum += i; // this will sum the natural numbers and stored under variable name sum
 }
 console.log("sum of " + n + " natural numbers" + sum); // sum will print out of loop , so it can print once , if we use it under the loop then sum is also print n number of times
 
+// print cube of n natural number
 
-// print cube of n natural number 
+let cube = 0; // initialize cube
+let n = parseInt(prompt("enter any digit")); // parseInt is used to covert string to number
+for (i = 0; i <= n; i++) {
+  cube = i * i * i; // it can 3 times the i values
+}
+console.log("cube of " + n + " is " + cube);
 
-let cube = 0; // initialize cube 
-let n = parseInt(prompt("enter any digit")); // parseInt is used to covert string to number 
-for( i=0;i<=n;i++){
-    cube=i*i*i; // it can 3 times the i values
-}console.log("cube of " + n + " is " + cube);
+// cube tabel
+let cube = 0; // initialize cube
+let n = parseInt(prompt("enter any digit")); // parseInt is used to covert string to number
+for (i = 0; i <= n; i++) {
+  cube = i * i * i; // it can 3 times the i values
+
+  console.log("cube of " + i + " is " + cube);
+} // this will print the cube table
